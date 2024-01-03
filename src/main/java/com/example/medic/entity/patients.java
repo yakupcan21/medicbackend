3d3ff135c18aeb19656138b94802fd4cb0de9b3c
Yakup Özdemir <yakupozdemir2003@hotmail.com>
2024-01-03 16:25:27 +0300

    wip

--- src/main/java/com/example/medic/entity/patients.java
+++ src/main/java/com/example/medic/entity/patients.java
@@ -1,9 +1,10 @@
 package com.example.medic.entity;
 
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.math.BigInteger;
 
 @Getter
 @Setter
@@ -12,19 +13,32 @@ import lombok.Setter;
 public class patients {
     @Id
     @GeneratedValue
+    private long patientId;
     private String patientName;
-    private String patientId;
-    private String lastVisit;
-    private int reportNum;
+    private String patientSurname;
+    private String patientDateOfBirth;
+    private long patientPhoneNo;
+    private String patientPassword;
+    private int patientAge;
+    private int patientHeight;
+    private float patientWeight;
+    private float patientBmi;
 
+    @OneToOne(mappedBy = "patient", cascade = CascadeType.ALL)
+    private reports report;
 
     public patients() { super(); }
 
-    public patients(String patientName, String patientId, String lastVisit, int reportNum) {
-        this.patientName = patientName;
+    public patients(long patientId, String patientName, String patientSurname, String patientDateOfBirth, long patientPhoneNo, String patientPassword, int patientAge, int patientHeight, int patientWeight, int patientBmi ) {
         this.patientId = patientId;
-        this.lastVisit = lastVisit;
-        this.reportNum = reportNum;
-    }
-
+        this.patientName = patientName;
+        this.patientSurname = patientSurname;
+        this.patientDateOfBirth = patientDateOfBirth;
+        this.patientPhoneNo = patientPhoneNo;
+        this.patientPassword = patientPassword;
+        this.patientAge = patientAge;
+        this.patientHeight = patientHeight;
+        this.patientWeight = patientWeight;
+        this.patientBmi = patientBmi;
+    }
 }
--- src/main/java/com/example/medic/entity/reports.java
+++ src/main/java/com/example/medic/entity/reports.java
@@ -1,4 +1,32 @@
 package com.example.medic.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "reports")
 public class reports {
+    @Id
+    @GeneratedValue
+    private String rapDate;
+    private long rapNum;
+    private String doctorName;
+
+    @OneToOne
+    private patients patient;
+
+    public reports() { super(); }
+
+    public reports(String rapDate, long rapNum, String doctorName) {
+        this.rapDate = rapDate;
+        this.rapNum = rapNum;
+        this.doctorName = doctorName;
+    }
 }
